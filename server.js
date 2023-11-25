512407518ae0466e95fb9b627d133df1c82dc6cf
Shanmukhgangi <100065789+Shanmukhgangi@users.noreply.github.com>
2023-11-25 16:24:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,38 +3,43 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { config } from 'dotenv';
 import router from './router/route.js';
+
+/** import connection file */
 import connect from './database/conn.js';
 
 const app = express();
 
-// Load environment variables from .env file
-config();
-
-// Middleware
+/** app middlewares */
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+config();
 
-// Routes
-app.use('/api', router);
+/** application port */
+const port = process.env.PORT || 8080;
+
+/** routes */
+app.use('/api', router); /** APIs */
 
 app.get('/', (req, res) => {
-  try {
-    res.json("Get Request");
-  } catch (error) {
-    res.json(error);
-  }
+    try {
+        res.json("Get Request");
+    } catch (error) {
+        res.json(error);
+    }
 });
 
-// Start server only when we have a valid connection
-connect()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server connected to http://localhost:${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error);
+/** start server only when we have a valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error("Cannot connect to the server");
+    }
+}).catch(error => {
+    console.error("Invalid Database Connection");
     // Optionally, you can exit the process here if you want to prevent the server from starting
-    process.exit(1);
-  });
+    // process.exit(1);
+});
