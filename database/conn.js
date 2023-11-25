f1694030a13822289848435cf95f48ad43f94284
Shanmukhgangi <100065789+Shanmukhgangi@users.noreply.github.com>
2023-11-25 16:22:52 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,18 +1,14 @@
-// /opt/render/project/src/database/conn.js
+import mongoose from "mongoose";
 
-import mongoose from 'mongoose';
-
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    throw error; // Rethrow the error to handle it in your main script
-  }
-};
-
-export default connect;
+export default async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database Connected");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        throw error; // This will propagate the error to the caller
+    }
+}
